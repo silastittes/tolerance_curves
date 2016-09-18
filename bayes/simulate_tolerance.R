ff9c4d02b09f85e57b92a3c33cfa44f06ffc738b
silastittes <silas.tittes@gmail.com>
2016-09-18 12:33:09 -0600

    tweaking simulation data. attempting to find adjustment for zero scaling
    Former-commit-id: 2ba5d96197207220c3c989384d170921790eaf8f
    Former-commit-id: dca262d47022c2741814bc7921fa211f2ddfbab4
    Former-commit-id: c7afd1f1285bec483d46493976fb0d2d3a808023

--- bayes/simulate_tolerance.R
+++ bayes/simulate_tolerance.R
@@ -18,7 +18,7 @@ for (z in 1:simreps) {
   nu <- rgamma(1, 20, 0.2)
   a <- rtruncnorm(n = nSpp, mean = 4, sd = 1, a = 2)
   b <- rtruncnorm(n = nSpp, mean = 3, sd = 1, a = 2)
-  c <- rtruncnorm(n = nSpp, mean = 0, sd = 10, a = 0)
+  c <- rtruncnorm(n = nSpp, mean = 0, sd = 2, a = 0)
   d <- rtruncnorm(n = nSpp, mean = min(x_o), sd = 2, b = min(x_o))
   e <- rtruncnorm(n = nSpp, mean = max(x_o), sd = 2, a = max(x_o))
   e1 <- e - d
@@ -28,8 +28,8 @@ for (z in 1:simreps) {
   #beta_1 <- rtruncnorm(1, mean = 0, sd = 2, b = 0)
 
   
-  beta_0 <- rnorm(1, mean = 20, sd = 2)
-  beta_1 <- rtruncnorm(1, mean = 0, sd = 2, b = 0)
+  beta_0 <- rnorm(1, mean = 2, sd = 0.2)
+  beta_1 <- rtruncnorm(1, mean = 0, sd = 0.2, b = 0)
   
   
   tseq <- seq(0,20, length.out = 1000)
@@ -50,25 +50,49 @@ for (z in 1:simreps) {
   #store data
   dimDat <- list(treat = rep(NA, nobs), spp = rep(NA, nobs), y = rep(NA,nobs))
 
+  
+#approximate zero gamma sampling
   drow <- 1
   for (i in 1:nSpp) {
     for (j in 1:ntreat) {
       for (k in 1:nSppTreat) {
         dimDat$treat[drow] <- x_o[j]
         dimDat$spp[drow] <- i
-        is_zero <- rbinom(1, 1, p_zero[j, i])
-        if (is_zero) {
-          dimDat$y[drow] <- 0
-        } else {
-          dimDat$y[drow] <- rgamma(n = 1, shape = nu, rate = nu / mus[j, i])
-        }
+        dimDat$y[drow] <- rgamma(n = 1, shape = nu, 
+                                 rate = (p_zero[j, i]*nu) / mus[j, i])
         drow <- drow + 1
       }
     }
   }
+  
   #add to ydat matrix
   ydat[,z] <- dimDat$y
+
+  
 }
+
+  
+  
+#zero inflated sampling 
+#  drow <- 1
+#  for (i in 1:nSpp) {
+#    for (j in 1:ntreat) {
+#      for (k in 1:nSppTreat) {
+#        dimDat$treat[drow] <- x_o[j]
+#        dimDat$spp[drow] <- i
+#        is_zero <- rbinom(1, 1, p_zero[j, i])
+#        if (is_zero) {
+#          dimDat$y[drow] <- 0
+#        } else {
+#          dimDat$y[drow] <- rgamma(n = 1, shape = nu, rate = nu / mus[j, i])
+#        }
+#        drow <- drow + 1
+#      }
+#    }
+#  }
+  #add to ydat matrix
+# ydat[,z] <- dimDat$y
+#}
 
 range(ydat)
 
@@ -123,8 +147,6 @@ for (i in 1:ndraw) {
 # add true
 lines(mu_vals, plogis(beta_0 + beta_1 * mu_vals), col = 'red', lwd = 3)
 
-
-
 #plot(jitter(dimDat$treat), dimDat$yScale,
 plot(jitter(dimDat$treat), dimDat$y,
      col = dimDat$spp, pch = 19,
@@ -138,9 +160,9 @@ for(i in 1:ndraw){
   for(j in 1:ncol(post$e1)){
     xdat1 <- xseq*post$e1[i,j] + post$d[i,j]
     ydat1 <- stretch.kumara(xseq, post$a[i,j], post$b[i,j], post$c[i,j])
-  
+    
     lines(xdat1, ydat1, col=j)
-  
+    
   }
 }
 
@@ -152,7 +174,6 @@ for(i in 1:nSpp){
   lines(xdat1, ydat1, col="cyan", lwd=2)
 }
 
-## ADD SMOOTH SPLINE CURVE
 
 slen <- unique(dimDat$spp)
 for(i in slen){
@@ -168,24 +189,28 @@ points(jitter(dimDat$treat), dimDat$y,
 plotInt <- function(param, d, value){
   
   di <- dim(param)
+  par_name <- deparse(substitute(param))
+  only_name <- unlist(strsplit(x = par_name, split = "\\$"))[2]
+  main_par <- paste(only_name, d)
   
   if( length(di) < 2){
     pDen <- density(param)
-    plot(pDen)
+    plot(pDen, main = main_par)
     high <- max(pDen$y)*0.2
     qs <- quantile(param, c(0.025, 0.5, 0.975))
     pMean <- mean(param)
   } else {
     pDen <- density(param[,d])
-    plot(pDen)
+    plot(pDen, main = main_par)
     high <- max(pDen$y)*0.2
     qs <- quantile(param[,d], c(0.025, 0.5, 0.975))
     pMean <- mean(param[,d])
   }
   
-  segments(x0 = value[d], y0 = 0, x1 = value[d], y1 = high, col="blue", lwd=4)
+
   segments(x0 = qs, y0 = 0, x1 = qs, y1 = high, col="red", lwd=4)
   segments(x0 = pMean, y0 = 0, x1 = pMean, y1 = high, col="green", lwd=4)
+  segments(x0 = value[d], y0 = 0, x1 = value[d], y1 = high, col="blue", lwd=4)
   
   legend("topleft", c("true", "95% cred int", "mean"), 
          lwd=3, col=c("blue","red", "green"), bty="n", cex = 1)
@@ -213,14 +238,20 @@ plotInt(param = post$beta_1, d = 1, value = beta_1)
 
 
 meanDraws <- apply(X = post$mu, 2, mean)
-#plot(emery$Inflor_biomass, meanDraws)
-#abline(0,1, lty=2, col="red")
 plot(meanDraws, ydat - meanDraws,
      pch=as.integer(dimDat$spp))
-
-#abline(h=0)
-
 smth <- smooth.spline(meanDraws, ydat - meanDraws)
 lines(smth$x, smth$y)
 
-by(data = dimDat$y, INDICES = dimDat$spp, FUN = function(x) sum(x==0))
+by(data = dimDat$y, 
+   INDICES = list(species = dimDat$spp, trt = dimDat$treat), 
+   FUN = function(x) max(x) > 0 )
+
+
+
+cl <- 1
+mcmc_p <- post$b[, cl]
+t_p <- a[cl]
+mcmc_q <- quantile(mcmc_p, c(0.025, 0.975))
+t_p
+mcmc_q[1] < t_p & t_p < mcmc_q[2]
